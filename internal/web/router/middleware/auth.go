645aec96b85054dc5d867891bd66f16ba5d849d0
André Meireles <andre2meireles@gmail.com>
2024-01-08 19:58:06 -0300

    Allow auth too

--- internal/web/router/middleware/auth.go
+++ internal/web/router/middleware/auth.go
@@ -8,18 +8,31 @@ import (
 	"github.com/andresmeireles/speaker/internal/modules/auth"
 )
 
+func getToken(req *http.Request) (string, error) {
+	if auth := req.Header.Get("Authorization"); auth != "" {
+		return auth, nil
+	}
+
+	cookie, err := req.Cookie("session_id")
+	if err != nil {
+		return "", err
+	}
+
+	return cookie.Value, nil
+}
+
 func CheckTokenOnCookie(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
-		cookie, err := request.Cookie("session_id")
+		token, err := getToken(request)
 		if err != nil {
-			slog.Error("error on cookie", "cookie", err)
+			slog.Error("error on cookie or auth", "cookie", err)
 			response.WriteHeader(http.StatusBadRequest)
 			response.Write([]byte("Token not found"))
 
 			return
 		}
 		authActions := auth.NewActions()
-		authEntity, err := auth.AuthRepository{}.GetByHash(cookie.Value)
+		authEntity, err := auth.AuthRepository{}.GetByHash(token)
 		if err != nil {
 			slog.Error("error on repository", "cookie", err)
 			unauthorized(response)
